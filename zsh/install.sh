bb6964c08aa543f78faf9562913afce6f80b2bd7
Jonathon E. Corrales de Oliveira <jcjonathonc5@gmail.com>
2025-06-18 13:05:03 -0400

    automagic messaging from me in the past

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,23 +2,18 @@
 
 # Install script for zsh configuration
 
-# Set up paths
-DOTFILES_DIR="$HOME/.dotfiles"
-ZSH_DIR="$DOTFILES_DIR/.oh-my-zsh"
-
 echo "Setting up zsh configuration..."
 
 # Install oh-my-zsh if not present
-if [ ! -d "$ZSH_DIR" ]; then
-    echo "Installing Oh My Zsh to $ZSH_DIR..."
-    export ZSH="$ZSH_DIR"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 # Install zsh-syntax-highlighting plugin
-if [ ! -d "$ZSH_DIR/custom/plugins/zsh-syntax-highlighting" ]; then
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
     echo "Installing zsh-syntax-highlighting plugin..."
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_DIR/custom/plugins/zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 echo "✅ Zsh setup complete!"
