6a7266464353ad0f921287cfeb1365168bdf5c34
Jonathon E. Corrales de Oliveira <jcjonathonc5@gmail.com>
2025-06-18 15:26:30 -0400

    automagic messaging from me in the past

--- nvim/lua/lang/lsp.lua
+++ nvim/lua/lang/lsp.lua
@@ -133,6 +133,7 @@ return {
         { name = 'copilot', group_index = 2 },
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- For luasnip users.
+        { name = 'path' },
       }, {
         { name = 'buffer' },
       }),
