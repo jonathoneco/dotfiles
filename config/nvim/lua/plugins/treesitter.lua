506153787bfc7fc34573b97a276168420940b980
Jonathon Corrales de Oliveira <jon.cor.oli@gmail.com>
2025-10-23 17:17:41 -0400

    automagic messaging from me in the past

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -92,6 +92,9 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         lazy = false,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
         config = config,
     },
     {
