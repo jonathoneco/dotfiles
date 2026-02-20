d8210e4452a32bd99b7f5ac75a5f7f3cc4e72741
Jonathon Corrales de Oliveira <jon.cor.oli@gmail.com>
2026-02-20 04:19:57 -0500

    feat: add telescope grep for hidden files
    Leader+sG runs live_grep with --hidden flag to search dotfiles
    and other hidden directories.

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -98,6 +98,11 @@ return {
                 builtin.grep_string({ search = word })
             end, { desc = "[S]earch current [W]ORD" })
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+            vim.keymap.set("n", "<leader>sG", function()
+                builtin.live_grep({
+                    additional_args = { "--hidden" },
+                })
+            end, { desc = "[S]earch by [G]rep (including hidden)" })
             vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
             vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
